0a70dcd3c35efedb91855a2da4174d092d978f49
Punit Bhadja <punit@helloryse.com>
2023-09-12 14:20:30 +0530

    Initial Commit

---
+++ Stocks/StocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksApp.swift
+//  Stocks
+//
+//  Created by Punit's MacBook on 12/09/23.
+//
+
+import SwiftUI
+
+@main
+struct StocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
